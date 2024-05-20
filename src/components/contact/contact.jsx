1d8388be71708de5d135ee82b0305d5ca17fab01
PpoSil <caca30@naver.com>
2024-05-20 05:45:46 +0000

    기본 템플릿 css 및 파비콘 삭제

--- src/components/contact/contact.jsx
+++ src/components/contact/contact.jsx
@@ -5,16 +5,6 @@ const contact = () => {
   return (
     <section>
       <div>
-        <h3>벌이 도망갔습니다...</h3>
-        <img className={styles["notfound-img"]} src="/not-found.jpg" alt="" />
-        <p>
-          <a href="https://kr.freepik.com/free-vector/cute-honey-bee-flying-cartoon-vector-icon-illustration-animal-nature-icon-concept-isolated-premium_34068066.htm#fromView=search&page=1&position=10&uuid=878dfc4f-393b-4132-a072-9f83a24df8b2">
-            작가 catalyststuff 출처 Freepik
-          </a>
-        </p>
-      </div>
-
-      <div>
         <img
           src="/solbee.jpg"
           alt="The Bee Is Gone Away!!"
